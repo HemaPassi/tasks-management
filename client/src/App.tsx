8f470fe085cad625a323c30f2bc6bb020d6572b0
Hema Passi <hema.passi@gmail.com>
2026-03-04 22:38:37 +0530

    deployment issue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,9 @@ import TodoForm from "./components/TodoForm"
 import TodoList from "./components/TodoList"
 import './App.css'
 
-export const BASE_URL = "http://localhost:5000/api"
+//export const BASE_URL = "http://localhost:5000/api"
+//export const BASE_URL = import.meta.env.VITE_API_URL;
+export const BASE_URL = "https://tasks-management-hfy0.onrender.com/api"
 
 
 function App() {
